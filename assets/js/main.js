255aebe8def01cb27664194157e6a4a474a17c33
Masputrawae <masputrawae.official@gmail.com>
2025-02-02 01:19:20 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,3 +76,33 @@ switchTheme.addEventListener("click", () => {
   const newTheme = currentTheme === "dark" ? "light" : "dark";
   applyTheme(newTheme);
 });
+
+// TABS
+document.addEventListener("DOMContentLoaded", function () {
+  function setupTabs(group) {
+    const selector = document.querySelector(`.tab-selector[data-tabs-group="${group}"]`);
+    const tabContents = document.querySelectorAll(`.tab-area .tab-area__content[data-tab][data-tabs-group="${group}"]`);
+    
+    if (!selector) return;
+    
+    selector.addEventListener("change", function () {
+      const selectedTab = this.value;
+      
+      tabContents.forEach((tab) => {
+        if (tab.getAttribute("data-tab") === selectedTab) {
+          tab.classList.add("tab-area__content--active");
+        } else {
+          tab.classList.remove("tab-area__content--active");
+        }
+      });
+    });
+  }
+
+  // Temukan semua elemen selector yang memiliki atribut data-tabs-group
+  const tabGroups = new Set(
+    [...document.querySelectorAll(".tab-selector[data-tabs-group]")].map(el => el.getAttribute("data-tabs-group"))
+  );
+  
+  // Pasang event listener untuk setiap grup tab
+  tabGroups.forEach(group => setupTabs(group));
+});
